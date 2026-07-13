929131b3e746835e25c5c46fddbddfae7ff4600a
jsmblog <jsmblog19@gmail.com>
2026-07-12 20:22:11 -0500

    fix corsoption

--- src/config/corsOption.js
+++ src/config/corsOption.js
@@ -1,5 +1,5 @@
 export const corsOption = {
-  origin: ['http://localhost:8100', 'http://localhost:3000','http://localhost:5173', 'https://gaia-corp.netlify.app' , 'https://inversion360.netlify.app/'],
+  origin: ['http://localhost:8100', 'http://localhost:3000','http://localhost:5173', 'https://gaia-corp.netlify.app' , 'https://inversion360.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
